0352ea76dd932cab6d4fd4bf0581e86b55909333
liuyonghui <18435162263@163.com>
2026-06-06 10:19:16 +0800

    .432580377982908:d467c81af22ed2905ffff5d16a65d162_6a238274c17ca316204b7b23.6a23827dc17ca316204b7b25.6a23827d02e2a55f2c0df047:Trae CN.T(2026/6/6 10:14:21)

--- backend/apps/knowledge_graph/signals.py
+++ backend/apps/knowledge_graph/signals.py
@@ -1,8 +1,20 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 from django.db import transaction
 from apps.books.models import Book, LoanRecord
 from .models import BookRelation
+
+_previous_status = {}
+
+
+@receiver(pre_save, sender=LoanRecord)
+def capture_previous_status(sender, instance, **kwargs):
+    if instance.pk:
+        try:
+            old_instance = LoanRecord.objects.get(pk=instance.pk)
+            _previous_status[instance.pk] = old_instance.status
+        except LoanRecord.DoesNotExist:
+            _previous_status[instance.pk] = None
 
 
 @receiver(post_save, sender=Book)
@@ -34,7 +46,17 @@ def create_author_relations(sender, instance, created, **kwargs):
 
 @receiver(post_save, sender=LoanRecord)
 def create_loan_based_relations(sender, instance, created, **kwargs):
-    if not created or instance.status not in ['borrowed', 'returned']:
+    old_status = _previous_status.pop(instance.pk, None)
+
+    if created:
+        status_just_became_active = instance.status in ['borrowed', 'returned']
+    else:
+        status_just_became_active = (
+            old_status not in ['borrowed', 'returned']
+            and instance.status in ['borrowed', 'returned']
+        )
+
+    if not status_just_became_active:
         return
 
     user = instance.user
